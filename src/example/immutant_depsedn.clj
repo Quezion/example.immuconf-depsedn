9da428f072f23599f77f9e23ced7cd62232d79ce
Quest Yarbrough (*) <qyarbrough@GR-KCX30HW4Vmbp.local>
2022-01-27 15:31:38 -0800

    Error repros with clojure -M:uberjar
    Execution error at uberdeps.api/fn (api.clj:108).
    No dispatch macro for: ?

---
+++ src/example/immutant_depsedn.clj
@@ -0,0 +1,14 @@
+(ns example.immutant-depsedn
+  (:require [uap-clj.browser :as uab])
+  (:gen-class))
+
+(defn greet
+  "Callable entry point to the application."
+  [data]
+  (println
+   (uab/browser "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36")))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (greet {:name (first args)}))
